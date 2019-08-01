0dfe907491aed7764920a3bc4872fced5308b518
James Brouillet <jamesbrouillet@Jamess-MacBook-Pro.local>
2019-07-31 23:39:29 -0700

    Changed camera settings

--- Scenes/Camera/Camera2D.gd
+++ Scenes/Camera/Camera2D.gd
@@ -4,14 +4,27 @@ var cameraPosition
 const CAMERA_CURSOR_DIFFERENTIAL_FACTOR = 3
 
 func _on_Cursor_cursorMoved(direction, cursor_position):
+	debug(cursor_position)
 	match direction:
 		"up":
-			if abs($"Camera2D".position.y - cursor_position.y) <= (Cell.CELL_SIZE * CAMERA_CURSOR_DIFFERENTIAL_FACTOR):
+			if abs($"Camera2D".position.y - cursor_position.y) >= (Cell.CELL_SIZE * CAMERA_CURSOR_DIFFERENTIAL_FACTOR):
 				$"Camera2D".position.y -= Cell.CELL_SIZE
 		"down":
-			if abs($"Camera2D".position.y - cursor_position.y) <= (Cell.CELL_SIZE * CAMERA_CURSOR_DIFFERENTIAL_FACTOR):
+			if abs(cursor_position.y - $"Camera2D".position.y) >= (Cell.CELL_SIZE * CAMERA_CURSOR_DIFFERENTIAL_FACTOR):
 				$"Camera2D".position.y += Cell.CELL_SIZE
+		"left":
+			if abs(cursor_position.x - $"Camera2D".position.x) <= (Cell.CELL_SIZE * CAMERA_CURSOR_DIFFERENTIAL_FACTOR):
+				$"Camera2D".position.x -= Cell.CELL_SIZE
+		"right":
+			if abs($"Camera2D".position.x - cursor_position.x) <= (Cell.CELL_SIZE * CAMERA_CURSOR_DIFFERENTIAL_FACTOR):
+				$"Camera2D".position.x += Cell.CELL_SIZE
 
 func _on_Level_mapInformationLoaded():
 	$"Camera2D".limit_bottom = get_parent().get_node("Level").map_width * Cell.CELL_SIZE
-	$"Camera2D".limit_right = get_parent().get_node("Level").map_height * Cell.CELL_SIZE
+	$"Camera2D".limit_right = get_parent().get_node("Level").map_height * Cell.CELL_SIZE - 32
+
+func debug(cursor_position):
+	var x = abs($"Camera2D".position.x - cursor_position.x)
+	var y = abs($"Camera2D".position.y - cursor_position.y)
+	print("Camera Position ", $"Camera2D".position)
+	print("Difference in position X: {x} Y: {y}".format({"x": x, "y" : y}))
--- Scenes/GUI/Cursor/Cursor.gd
+++ Scenes/GUI/Cursor/Cursor.gd
@@ -15,26 +15,28 @@ func _input(event):
 	# Move Cursor by x pixels
 	if Input.is_action_pressed("ui_left"):
 		self.position.x -= Cell.CELL_SIZE
+		preventOutOfBounds()
 		emit_signal("cursorMoved", "left", self.position)
 	elif Input.is_action_pressed("ui_right"):
 		self.position.x += Cell.CELL_SIZE
+		preventOutOfBounds()
 		emit_signal("cursorMoved", "right", self.position)
 	elif Input.is_action_pressed("ui_down"):
 		self.position.y += Cell.CELL_SIZE
+		preventOutOfBounds()
 		emit_signal("cursorMoved", "down", self.position)
 	elif Input.is_action_pressed("ui_up"):
 		self.position.y -= Cell.CELL_SIZE
+		preventOutOfBounds()
 		emit_signal("cursorMoved", "up", self.position)
 	
-	# Prevent the cursor from going out of bounds
-	preventOutOfBounds()
-	
-	#debug()
+	if event is InputEventKey:
+		debug()
 
 func preventOutOfBounds():
-	self.position.x = clamp(self.position.x, 0, get_parent().get_node("Level").map_width * Cell.CELL_SIZE)
-	self.position.y = clamp(self.position.y, 0, get_parent().get_node("Level").map_height * Cell.CELL_SIZE)
+	self.position.x = clamp(self.position.x, 0, (get_parent().get_node("Level").map_height * Cell.CELL_SIZE) - (Cell.CELL_SIZE * 3))
+	self.position.y = clamp(self.position.y, 0, (get_parent().get_node("Level").map_width * Cell.CELL_SIZE) - Cell.CELL_SIZE)
 	
 
 func debug():
-	print(self.position)
+	print("Cursor is at: ", self.position)
