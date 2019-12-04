11bebbf95b616dc5efbb80e64aa25439dd99862d
James Brouillet <james_brouillet@hotmail.com>
2019-12-03 19:27:19 -0800

    Started Natasha Cleric

---
+++ Scenes/Units/Player_Units/AllyUnits/Natasha/Natasha.gd
@@ -0,0 +1,20 @@
+extends Battlefield_Unit
+
+func _ready():
+	# Initial Animation
+	$Animation.current_animation = "Idle"
+	
+	# Set this when the level loads but for now, this is just a test to simply things
+	UnitStats.name = "Natasha"
+	
+	# Unit Portrait
+	unit_portrait_path = preload("res://assets/units/cleric/natasha portrait.png")
+	
+	# Unit Mugshot
+	unit_mugshot = preload("res://assets/units/cleric/natasha mugshot.png")
+	
+	# Add Healing Staff
+	UnitInventory.add_item(preload("res://Scenes/Items/Swords/Iron Sword.tscn").instance())
+	
+	# Set combat node
+	combat_node = preload("res://Scenes/Units/Player_Units/AllyUnits/Eirika/Eirika Combat.tscn")
