ab6720967223ff5854e944a1c2475d54fe8a4074
Mike Kendrick <mikek@mach112.local>
2020-01-21 20:36:00 -0800

    Started dark mage unit

---
+++ Scenes/Units/Enemy_Units/Dark Mage Enemy.gd
@@ -0,0 +1,22 @@
+extends Battlefield_Unit
+
+func _ready():
+	# Initial Animation
+	$Animation.current_animation = "Idle"
+	
+	# Set this when the level loads but for now, this is just a test to simply things
+	UnitStats.name = "Dark evil magician"
+	
+	# Unit Portrait
+	unit_portrait_path = preload("res://assets/units/pegasus knight/Vanessa Portrait.png")
+	
+	# Unit Mugshot
+	unit_mugshot = unit_portrait_path
+	
+	# Lance
+	UnitInventory.usable_weapons.append(Item.WEAPON_TYPE.LANCE)
+	var lance = preload("res://Scenes/Items/Lance/Iron Lance.tscn").instance()
+	UnitInventory.add_item(lance)
+	
+	# Set combat node
+	combat_node = preload("res://Scenes/Units/Player_Units/AllyUnits/Ally Pegasus Knight/Vanessa Combat.tscn")
