60a2d0be240bb26a5f47d1415c926933f6265201
James Brouillet <jamesbrouillet@Jamess-MacBook-Pro.local>
2020-01-21 22:41:32 -0800

    Changed dark mage

--- Scenes/Units/Enemy_Units/Dark Mage Enemy.gd
+++ Scenes/Units/Enemy_Units/Dark Mage Enemy.gd
@@ -1,22 +1,20 @@
 extends Battlefield_Unit
 
+# Called when the node enters the scene tree for the first time.
 func _ready():
-	# Initial Animation
 	$Animation.current_animation = "Idle"
+	UnitMovementStats.is_ally = false
 	
-	# Set this when the level loads but for now, this is just a test to simply things
-	UnitStats.name = "Dark evil magician"
+	# Portrait
+	unit_portrait_path = preload("res://assets/units/enemyPortrait/Main Villain MugShot.png")
 	
-	# Unit Portrait
-	unit_portrait_path = preload("res://assets/units/pegasus knight/Vanessa Portrait.png")
+	# Mug shot
+	unit_mugshot = preload("res://assets/units/enemyPortrait/Main Villain MugShot.png")
 	
-	# Unit Mugshot
-	unit_mugshot = unit_portrait_path
-	
-	# Lance
+	# Inventory and Weapons
+	UnitInventory.usable_weapons.append(Item.WEAPON_TYPE.AXE)
 	UnitInventory.usable_weapons.append(Item.WEAPON_TYPE.LANCE)
-	var lance = preload("res://Scenes/Items/Lance/Iron Lance.tscn").instance()
-	UnitInventory.add_item(lance)
+	UnitInventory.add_item(preload("res://Scenes/Items/Axes/Gorehowl.tscn").instance())
 	
-	# Set combat node
-	combat_node = preload("res://Scenes/Units/Player_Units/AllyUnits/Ally Pegasus Knight/Vanessa Combat.tscn")
+	# Combat sprite
+	combat_node = preload("res://Scenes/Units/Enemy_Units/Enemy General Black Combat.tscn")
