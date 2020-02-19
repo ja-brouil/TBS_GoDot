e5c1ae46b235accd20442181a718e08f1a1292d5
James Brouillet <james_brouillet@hotmail.com>
2020-02-19 13:36:56 -0800

    Added few more things to the unit select

--- assets/UI/Unit Picker Screen/Unit Select.gd
+++ assets/UI/Unit Picker Screen/Unit Select.gd
@@ -6,18 +6,24 @@ var max_units_select_possible = 6
 # Array for all units selected
 var units_selected = []
 
+# Current index
+var current_index = 0
+
 # Node access
-onready var item_list = $"Unit List"
+onready var unit_list = $"Unit List"
 
 func _ready():
 	# Disable the scroll bar
-	var scroll_bar = item_list.get_v_scroll()
+	var scroll_bar = unit_list.get_v_scroll()
 	scroll_bar.mouse_filter = Control.MOUSE_FILTER_IGNORE
 	scroll_bar.modulate = Color(1,1,1,0)
 	
 	# Get focus
-	item_list.select(0)
-	item_list.grab_focus()
+	unit_list.select(0)
+	unit_list.grab_focus()
+	
+	# Test
+	start(ItemList.SELECT_MULTI)
 
 func _input(event):
 	pass
@@ -25,7 +31,7 @@ func _input(event):
 # Start
 func start(select_mode_value):
 	# Set type
-	item_list.select_mode = select_mode_value
+	unit_list.select_mode = select_mode_value
 	
 	# Erase values
 	reset()
@@ -44,7 +50,11 @@ func populate_selected_units():
 	# Get all the units in the y sort on the battlefield
 	# For each of their ID, add them to the list
 	# Find eirika and make sure she is NOT selectable
-	
+	for unit in range(unit_list.get_item_count()):
+		if unit_list.get_item_text(unit) == "Eirika":
+			unit_list.set_item_selectable(unit, false)
+			units_selected.append(unit_list.get_item_text(unit))
+			return
 	# Add eirika to the list
 	# Add whoever else per chapter needs to be in
 	pass
@@ -56,3 +66,13 @@ func reset():
 	
 	# Clear the list
 	# item_list.clear()
+	
+	# Reset current index
+	current_index = 0
+
+
+
+func _on_Unit_List_item_selected(index):
+	# Is this Eirika?
+	if unit_list.get_item_text(index) == "Eirika":
+		unit_list.select(index)
